37e730103e7016b53c9e92769330edadfb07c140
josiahhopkins <hopkinsjosiah@gmail.com>
2017-05-11 11:16:21 -0700

    Started Registration page.

--- app/src/main/java/com/example/josiah/stockplayground/LoginFragment.java
+++ app/src/main/java/com/example/josiah/stockplayground/LoginFragment.java
@@ -18,50 +18,48 @@ import java.net.URLEncoder;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link RegisterFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link RegisterFragment#newInstance} factory method to
+ * Use the {@link LoginFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class RegisterFragment extends Fragment {
+public class LoginFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String LOGIN_URL = "http://cssgate.insttech.washington.edu/~josiah3/PHP_Code/PHP%20Code/login.php?";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    private EditText mUsernameEditText;
-    private EditText mPasswordEditText;
 
-    private final String USER_ADD_URL = "http://cssgate.insttech.washington.edu/~josiah3/PHP_Code/PHP%20Code/adduser.php?";
+    private loginListener mListener;
+    private EditText mUsernameLoginEdit;
+    private EditText mPasswordLoginEdit;
 
-    private View mProgressView;
-    private View mLoginFormView;
 
-    private UserAddListener mListener;
-
-    public interface UserAddListener{
-        public void addUser(String url);
-    }
-
-    public RegisterFragment() {
+    public LoginFragment() {
         // Required empty public constructor
-
     }
 
+    public interface loginListener {
+        void login(String url);
+    }
+
+    public interface registerListener {
+        void goToRegister();
+    }
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment RegisterFragment.
+     * @return A new instance of fragment LoginFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static RegisterFragment newInstance(String param1, String param2) {
-        RegisterFragment fragment = new RegisterFragment();
+    public static LoginFragment newInstance(String param1, String param2) {
+        LoginFragment fragment = new LoginFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -75,41 +73,36 @@ public class RegisterFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
-
-    private String buildCourseURL(View v) {
-        StringBuilder sb = new StringBuilder(USER_ADD_URL);
-        try {
-            String username = mUsernameEditText.getText().toString();
-            sb.append("username=");
-            sb.append(username);
-            String password = mPasswordEditText.getText().toString();
-            sb.append("&password=");
-            sb.append(URLEncoder.encode(password, "UTF-8"));
-            Log.i("CourseAddFragment", sb.toString());
-        } catch (Exception e) {
-            Toast.makeText(v.getContext(), "Something wrong with the url" + e.getMessage(), Toast.LENGTH_LONG).show();
         }
-        return sb.toString();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_register, container, false);
-        Button register_button = (Button) view.findViewById(R.id.sign_up_button);
-        register_button.setOnClickListener(new View.OnClickListener() {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        Button loginButton = (Button) view.findViewById(R.id.login_button);
+        Button registerButton = (Button) view.findViewById(R.id.register_button);
+        mPasswordLoginEdit = (EditText) view.findViewById(R.id.password_login_edit);
+        mUsernameLoginEdit = (EditText) view.findViewById((R.id.username_login_edit));
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String url = buildAddUserURL(v);
-                mListener.addUser(url);
+                String url = buildLoginURL(v);
+                mListener.login(url);
             }
         });
-        mUsernameEditText = (EditText) view.findViewById(R.id.register_username_edit);
-        mPasswordEditText = (EditText) view.findViewById(R.id.password_register_edit);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+                if(v.getId() == R.id.register_button){
+                    getFragmentManager().beginTransaction().replace(R.id.login_page_container, new RegisterFragment()).addToBackStack(null).commit();
+                }
+            }
+        });
         return view;
     }
 //
@@ -123,8 +116,8 @@ public class RegisterFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof UserAddListener) {
-            mListener = (UserAddListener) context;
+        if (context instanceof loginListener) {
+            mListener = (loginListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -137,28 +130,28 @@ public class RegisterFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
+//    /**
+//     * This interface must be implemented by activities that contain this
+//     * fragment to allow an interaction in this fragment to be communicated
+//     * to the activity and potentially other fragments contained in that
+//     * activity.
+//     * <p>
+//     * See the Android Training lesson <a href=
+//     * "http://developer.android.com/training/basics/fragments/communicating.html"
+//     * >Communicating with Other Fragments</a> for more information.
+//     */
+//    public interface OnFragmentInteractionListener {
+//        // TODO: Update argument type and name
+//        void onFragmentInteraction(Uri uri);
+//    }
 
-    private String buildAddUserURL(View v) {
-        StringBuilder sb = new StringBuilder(USER_ADD_URL);
+    private String buildLoginURL(View v) {
+        StringBuilder sb = new StringBuilder(LOGIN_URL);
         try {
-            String user = mUsernameEditText.getText().toString();
+            String user = mUsernameLoginEdit.getText().toString();
             sb.append("username=");
             sb.append(user);
-            String password = mPasswordEditText.getText().toString();
+            String password = mPasswordLoginEdit.getText().toString();
             sb.append("&password=");
             sb.append(URLEncoder.encode(password, "UTF-8"));
             Log.i("Login", sb.toString());
--- app/src/main/java/com/example/josiah/stockplayground/MainActivity.java
+++ app/src/main/java/com/example/josiah/stockplayground/MainActivity.java
@@ -1,13 +1,137 @@
 package com.example.josiah.stockplayground;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity  implements RegisterFragment.UserAddListener, LoginFragment.loginListener, LoginFragment.registerListener{
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null
+        /* || getSupportFragmentManager().findFragmentById(R.id.list) == null
+        */
+        ) {
+            LoginFragment courseFragment = new LoginFragment();
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.login_page_container, courseFragment)
+                    .commit();
+        }
+
+    }
+
+    @Override
+    public void addUser(String url) {
+        AddUserTask task = new AddUserTask();
+        task.execute(url.toString());
+        // Takes you back to the previous fragment by popping the current fragment out.
+        getSupportFragmentManager().popBackStackImmediate();
+    }
+
+    @Override
+    public void login(String url) {
+        AddUserTask task = new AddUserTask();
+        task.execute(url.toString());
+        // Takes you back to the previous fragment by popping the current fragment out.
+        getSupportFragmentManager().popBackStackImmediate();
+        Intent i = new Intent();
+    }
+
+
+    @Override
+    public void goToRegister() {
+            Toast.makeText(this, "Register Clicked", Toast.LENGTH_SHORT).show();
+            getFragmentManager().beginTransaction().replace(R.id.login_page_container, new RegisterFragment()).addToBackStack(null).commit();
+    }
+
+
+    private class AddUserTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            for (String url : urls) {
+                try {
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+                    InputStream content = urlConnection.getInputStream();
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+                } catch (Exception e) {
+                    response = "Unable to add course, Reason: " + e.getMessage();
+                } finally {
+                    if (urlConnection != null) urlConnection.disconnect();
+                }
+            }
+            return response;
+        }
+
+
+        /**
+         * It checks
+         * to see if
+         * there was
+         * a problem
+         * with the
+         * <p>
+         * URL(Network) which is when an      *
+         * exception is
+         * caught.It tries
+         * to call
+         * the parse
+         * Method and
+         * checks to
+         * see if
+         * it was
+         * successful.      *
+         * If not, it
+         * displays the
+         * exception.      **
+         *
+         * @param result
+         */
+
+        @Override
+        protected void onPostExecute(String result) {
+            // Something wrong with the network or the URL.
+            try {
+                Log.v("onPostExecute", result);
+                JSONObject jsonObject = new JSONObject(result);
+                String status = (String) jsonObject.get("result");
+                if (status.equals("success")) {
+                    Toast.makeText(getApplicationContext(), "Success!!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Failed to add: " + jsonObject.get("error"), Toast.LENGTH_LONG).show();
+                }
+            } catch (JSONException e) {
+                Toast.makeText(getApplicationContext(), "Something wrong with the data" + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
